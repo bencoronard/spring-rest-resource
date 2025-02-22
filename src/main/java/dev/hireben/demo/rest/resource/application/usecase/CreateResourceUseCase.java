b2a5870fe77a2df30a31ec0bc4ce97725b8482f8
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-22 23:53:54 +0700

    Update CreateResourceUseCase.java

--- src/main/java/dev/hireben/demo/rest/resource/application/usecase/CreateResourceUseCase.java
+++ src/main/java/dev/hireben/demo/rest/resource/application/usecase/CreateResourceUseCase.java
@@ -3,9 +3,7 @@ package dev.hireben.demo.rest.resource.application.usecase;
 import java.time.Instant;
 
 import dev.hireben.demo.rest.resource.application.dto.CreateResourceDTO;
-import dev.hireben.demo.rest.resource.application.dto.ResourceDTO;
 import dev.hireben.demo.rest.resource.application.dto.UserDTO;
-import dev.hireben.demo.rest.resource.application.mapper.ResourceMapper;
 import dev.hireben.demo.rest.resource.domain.entity.Resource;
 import dev.hireben.demo.rest.resource.domain.repository.ResourceRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,20 +21,18 @@ public class CreateResourceUseCase {
   // Methods
   // ---------------------------------------------------------------------------//
 
-  public ResourceDTO createResource(CreateResourceDTO dto, UserDTO user) {
+  public Long create(CreateResourceDTO dto, UserDTO user) {
 
     Resource resource = Resource.builder()
         .field1(dto.getField1())
         .field2(dto.getField2())
         .field3(dto.getField3())
-        .tenant(user.getTenant())
         .createdBy(user.getId())
+        .tenant(user.getTenant())
         .createdAt(Instant.now())
         .build();
 
-    Resource savedResource = repository.save(resource);
-
-    return ResourceMapper.toDto(savedResource);
+    return repository.save(resource);
   }
 
 }
